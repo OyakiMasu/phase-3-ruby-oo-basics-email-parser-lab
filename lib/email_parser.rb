8506509d3ea081cfacd75a93985f5fd969e8b9a5
OyakMasu21 <akzaki17@gmail.com>
2023-02-13 20:21:01 +0300

    Completed assignment

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,8 +4,13 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-    def initalize
-        
+    attr_accessor :emails
+
+    def initialize (emails)
+        @emails = emails
     end
 
+    def parse
+        emails.split(/, | /).uniq
+    end
 end
